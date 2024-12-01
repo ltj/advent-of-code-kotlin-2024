3aeed7de6d778b654c3eaf292239997dfab5c04a
Lars Jacobsen <1213126+ltj@users.noreply.github.com>
2024-12-01 20:21:55 +0100

    December 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,34 @@
+import kotlin.math.abs
+
 fun main() {
+    fun asTwoLists(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val l1 = mutableListOf<Int>()
+        val l2 = mutableListOf<Int>()
+        input.forEach { s ->
+            val (a, b) = s.split("\\s+".toRegex(), 2).map { it.toInt() }
+            l1.add(a)
+            l2.add(b)
+        }
+        return Pair(l1, l2)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (l1, l2) = asTwoLists(input)
+        l1.sort()
+        l2.sort()
+        return l1.zip(l2).sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (l1, l2) = asTwoLists(input)
+        val memo = mutableMapOf<Int, Int>() // not strictly necessary memoization
+        return l1.sumOf { a -> memo.getOrPut(a, { l2.count { b -> b == a } }) * a }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
